cb4eab8f8a5a0153cd329c2332bdd692c8eeba3b
elsapet <ebraae@gmail.com>
2014-07-10 10:46:48 +0200

    Adding files

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Cv::Application.routes.draw do
 
   root :to => 'home#index'
-  get '/about', to: 'home#about', as: :about
-  get '/experience', to: 'home#experience', as: :experience
+  get '/about', to: 'home#index', as: :about
 
   get '/projects', to: 'projects#index', as: :projects
   get '/projects/industry', to: 'projects#industry_index', as: :industry_projects
